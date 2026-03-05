54bf24a7219d2805277eecee82b1d37741f1bd41
Mehdi <mehdi_sellam@hotmail.com>
2026-03-05 11:27:25 +0100

    Ajout du fichier principal

---
+++ backend/src/main/java/com/lab4life/Lab4lifeBackendApplication.java
@@ -0,0 +1,11 @@
+package com.lab4life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4lifeBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4lifeBackendApplication.class, args);
+    }
+}
